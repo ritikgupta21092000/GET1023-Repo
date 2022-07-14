9318445e24d5c83df1dcc60ea893c74022a47fc5
Ritik Gupta <guptaritik349@gmail.com>
2022-07-14 17:21:11 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Sample {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println("Adding One More Line");
+    System.out.println("Checking POLL SCM");
   }
 }
